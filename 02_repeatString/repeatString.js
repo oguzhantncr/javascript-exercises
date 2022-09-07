1c6048b59811e5b548af6a3adabfbd4812de1b15
oguzhantncr <oguzhan_tuncer14@icloud.com>
2022-09-07 11:35:25 +0200

    EX2 completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,21 @@
-const repeatString = function() {
+const repeatString = function(msg, repeat) {
+    let result = msg;
 
+    if (repeat == 0)
+    {
+        return ''
+    }
+    else if (repeat < 0)
+    {
+        return 'ERROR'
+    }
+
+    for (let i = 0; i < (repeat - 1); i++)
+    {
+        result += msg;
+    }
+
+    return result;
 };
 
 // Do not edit below this line
