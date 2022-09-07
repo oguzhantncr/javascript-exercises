735216f915483c8da800b58919fe3b4fd8ad8530
oguzhantncr <oguzhan_tuncer14@icloud.com>
2022-09-07 13:23:55 +0200

    EX4 completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    
+    let filtered = array;
 
+    for (let arg of args)
+    {
+        filtered = filtered.filter(function(value){return value !== arg});
+    }
+
+    return filtered
 };
 
 // Do not edit below this line
