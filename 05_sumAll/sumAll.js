e1db7a8f28969b31dce77d539050cb94361130a0
oguzhantncr <oguzhan_tuncer14@icloud.com>
2022-09-07 13:58:14 +0200

    EX5 completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(initial, last) {
+    let sum = 0
 
+    if (typeof initial !== "number" || initial < 0 ||
+        typeof last !== "number" || last < 0)
+        {
+            return 'ERROR';
+        }
+
+    if (initial > last)
+    {
+        [initial, last] = [last, initial]
+    }
+
+    for (let i = initial; i <= last; i++)
+    {   
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
