5bdced75acb54739085e293339e007a91b3e885c
ogi <oguzhan_tuncer14@icloud.com>
2024-06-12 00:50:13 +0200

    EX12 completed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,10 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+
+    ages = people.map(person => {
+        return person.yearOfDeath ? (person.yearOfDeath - person.yearOfBirth) : (Number(new Date().getFullYear()) - person.yearOfBirth);
+    });
+
+    return people[ages.indexOf(Math.max(...ages))];
 
 };
 
