f3735c08c103a657495c64f6d3062b98c4390e53
ogi <oguzhan_tuncer14@icloud.com>
2024-06-12 00:37:08 +0200

    EX11 completed

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,11 +1,5 @@
 const getTheTitles = function(books) {
-    let titles = [];
-
-    books.forEach(book => {
-        titles.push(book.title);
-    });
-
-    return titles;
+    return books.map(book => book.title);
 };
 
 // Do not edit below this line
