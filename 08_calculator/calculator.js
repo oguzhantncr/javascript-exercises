a4b52e166cca870b5948022f56041677a0cc01a5
ogi <oguzhan_tuncer14@icloud.com>
2024-06-11 23:42:16 +0200

    EX8 completed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(firstNum, secondNum) {
+  return firstNum + secondNum;
 };
 
-const subtract = function() {
-	
+const subtract = function(firstNum, secondNum) {
+  return firstNum - secondNum;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+	return nums.reduce((acc, curr) => {return acc + curr}, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  return nums.reduce((acc, curr) => {return acc * curr});
 };
 
-const power = function() {
-	
+const power = function(num, power) {
+	return num ** power;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  total = 1;
+	while (num > 0) {
+    total *= num;
+    num--;
+  }
+  return total;
 };
 
 // Do not edit below this line
