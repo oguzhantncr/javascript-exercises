124d079a99e638423ae8f17c5b81c43161f55f12
ogi <oguzhan_tuncer14@icloud.com>
2024-06-12 00:32:40 +0200

    EX10 completed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(nth) {
 
+    let count
+    if (typeof nth !== 'number') {
+        count = parseInt(nth)
+    } else {
+        count = nth
+    }
+
+    if (count < 0) return "OOPS";
+    if (count == 0) return 0;
+
+    let prev = 1;
+    let secondPrev = 0;
+    for (let i = 2; i <= count; i++) {
+        let current = prev + secondPrev;
+        secondPrev = prev;
+        prev = current;
+    }
+    return prev;
 };
 
 // Do not edit below this line
