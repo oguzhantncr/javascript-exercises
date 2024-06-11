a98a47837b8835a914dd4708d3d0c73b57f3a4ff
ogi <oguzhan_tuncer14@icloud.com>
2024-06-12 00:07:08 +0200

    EX9 completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
+const alphabet = [...'abcdefghijklmnopqrstuvwxyz'];
 
+const palindromes = function (word) {
+    reverse = word.toLowerCase().split('').filter((letter) => alphabet.includes(letter)).reverse().join('');
+    original = word.toLowerCase().split('').filter((letter) => alphabet.includes(letter)).join('');
+    return reverse === original;
 };
 
 // Do not edit below this line
